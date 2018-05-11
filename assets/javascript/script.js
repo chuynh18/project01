b3668e591cf90b78d4ce0be836b151bd3c026711
basosa94 <basosa@ucdavis.edu>
2018-05-11 10:56:50 -0700

    Add styling to html pages (change background images)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,79 @@
 "use strict";
+var placesResponse;
 
+var placesTextSearch = function() {
+    // this is where the magic on the client side happens
+    var searchQuery = $("#destinationSearch").val();
+    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + searchQuery + "&key=AIzaSyD6-UaTdmfPpw2x9P0Hf66Rl2XdzCwJvOQ&type=campground"
+    console.log(queryURL);
+    
+    // Creates AJAX call to Places API
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+
+        $("#cardsHere").empty();
+        $("#sampleParks").empty();
+
+        // console.log(response);
+        placesResponse = response;
+
+        var pois = $("<h2>");
+        pois.addClass("fredericka");
+        pois.html("<br>Points of Interest<br>");
+        $("#cardsHere").append(pois);
+
+        for (var i = 0; i < 8; i++) {
+            var bootstrapCard = $("<div>");
+            bootstrapCard.addClass("card col-md-3 ml-3 mr-3 mb-3 pt-3 attraction");
+            bootstrapCard.attr("style", "width: 18rem;");
+            bootstrapCard.attr("data-id", placesResponse.results[i].id);
+            bootstrapCard.attr("data-lat", placesResponse.results[i].geometry.location.lat);
+            bootstrapCard.attr("data-lng", placesResponse.results[i].geometry.location.lng);
+            
+            var cardBody = $("<div>");
+            cardBody.addClass("card-body");
+
+            var cardImg = $("<img>");
+            cardImg.addClass("card-img-top");
+            cardImg.attr("src", "https://maps.googleapis.com/maps/api/place/photo?maxwidth=350&photoreference=" + placesResponse.results[i].photos[0].photo_reference + "&key=AIzaSyD6-UaTdmfPpw2x9P0Hf66Rl2XdzCwJvOQ");
+            cardImg.attr("alt", placesResponse.results[i].name)
+
+            var cardTitle = $("<h5>");
+            cardTitle.addClass("card-title fredericka");
+            cardTitle.text(placesResponse.results[i].name);
+
+            cardBody.append(cardTitle);
+            bootstrapCard.append(cardImg);
+            bootstrapCard.append(cardBody);
+
+            $("#cardsHere").append(bootstrapCard);
+        };
+
+        // $("#cardsHere").text(JSON.stringify(response,null,'\t'));
+    });
+};
+
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
+document.getElementById("destinationSearch").onkeypress = function(event){
+    if (event.keyCode == 13 || event.which == 13){
+        placesTextSearch();
+    }
+};
